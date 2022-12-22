96fd6d3b98ac3f765a1f6f4ab374b0ea2d939aec
Zipri <kirill.serov.00@mail.ru>
2022-12-22 11:52:35 +0300

    1.5.0. bin: added game-gcd

--- bin/common-functions.js
+++ bin/common-functions.js
@@ -20,3 +20,17 @@ export const isTaskComplete = (question, result, name) => {
   log(`Let's try again, ${name}!`);
   return false;
 };
+
+// export const game = (name, greetings, result, question) => {
+//   log(greetings);
+//   let correctAnswers = 0;
+//
+//   while (correctAnswers < 3) {
+//     if (isTaskComplete(question, result, name)) {
+//       correctAnswers += 1;
+//     } else {
+//       correctAnswers = 100;
+//     }
+//   }
+//   if (correctAnswers === 3) log(`Congratulations, ${name}!`);
+// };
---
+++ bin/game-gcd.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+import { isTaskComplete, log, welcome } from './common-functions.js';
+
+const gcd = (x, y) => {
+  if (y > x) return gcd(y, x);
+  if (!y) return x;
+  return gcd(y, x % y);
+};
+
+const game = (name) => {
+  log('Find the greatest common divisor of given numbers.');
+  let correctAnswers = 0;
+
+  while (correctAnswers < 3) {
+    const operandA = Math.floor(Math.random() * 20);
+    const operandB = Math.floor(Math.random() * 20);
+    const result = gcd(operandA, operandB);
+    if (isTaskComplete(`Question: ${operandA} ${operandB}`, result, name)) {
+      correctAnswers += 1;
+    } else {
+      correctAnswers = 100;
+    }
+  }
+  if (correctAnswers === 3) log(`Congratulations, ${name}!`);
+};
+
+const start = () => {
+  const name = welcome();
+  game(name);
+};
+
+start();
